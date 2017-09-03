c15ac1ef0e7760efdc559da23ef937066ead2478
Yizhen Zhao <yizhen@uber.com>
2017-09-03 15:56:47 -0700

    Add registor url/template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent }          from './app.component';
+import { AppRoutingModule }      from './app-routing.module';
+import { UserRegistorComponent } from './user-registor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    UserRegistorComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-registor.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-user-registor',
+  templateUrl: './user-registor.component.html',
+})
+export class UserRegistorComponent {
+}
