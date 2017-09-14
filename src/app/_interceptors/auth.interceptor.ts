a0773e5a9ee354bbd84be12353ac63d9f1934b2d
Yizhen Zhao <yizhen@uber.com>
2017-09-14 14:35:22 -0700

    Add flashmessage when server is not reachable.

--- src/app/_interceptors/auth.interceptor.ts
+++ src/app/_interceptors/auth.interceptor.ts
@@ -1,8 +1,8 @@
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-import { Observable }       from 'rxjs/Observable';
-import { Injectable }       from '@angular/core';
+import { Observable }           from 'rxjs/Observable';
+import { Injectable }           from '@angular/core';
 import {
   HttpErrorResponse,
   HttpEvent,
@@ -10,17 +10,19 @@ import {
   HttpInterceptor,
   HttpRequest,
   HttpResponse,
-}                           from '@angular/common/http';
-import { Router }           from '@angular/router';
+}                               from '@angular/common/http';
+import { Router }               from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
-import { UserStateService } from '../_services';
+import { UserStateService }     from '../_services';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   constructor(
-    private router: Router,
-    private userState: UserStateService,
+    private router:                Router,
+    private userState:             UserStateService,
+    private flashMessagesService:  FlashMessagesService,
   ) {}
 
   intercept(
@@ -33,6 +35,12 @@ export class AuthInterceptor implements HttpInterceptor {
         if (err.status === 401 && err.error.message === 'require login') {
           this.userState.remove();
           this.router.navigate(['/login']);
+        } else if (err.statusText === 'Unknown Error') {
+          console.error('Server is not reachable');
+          // TODO: Figure out why flash message is not shown
+          this.flashMessagesService.show('Server is not reachable', {
+            timeout: 5000,
+          });
         }
       }
       return Observable.throw(err);
