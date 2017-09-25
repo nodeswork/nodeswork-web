5369a1f578b0774557fae99f71abe371bae2ab87
Yizhen Zhao <yizhen@uber.com>
2017-09-24 19:38:15 -0700

    Add update user applet support.

--- src/app/applets/user-applet-entry/user-applet-entry.component.ts
+++ src/app/applets/user-applet-entry/user-applet-entry.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { UserApplet }               from '../../_models';
+import {
+  UserApplet,
+}                                   from '../../_models';
+import {
+  UserAppletsService,
+}                                   from '../../_services';
 
 @Component({
   selector: 'app-user-applet-entry',
@@ -11,9 +16,22 @@ export class UserAppletEntryComponent implements OnInit {
 
   @Input() userApplet: UserApplet;
 
-  constructor() { }
+  constructor(
+    private userAppletsService: UserAppletsService,
+  ) { }
 
   ngOnInit() {
   }
 
+  async update() {
+    const updateApplet = {
+      config: this.userApplet.config,
+      enabled: true,
+    };
+    updateApplet.config.appletConfig = this.userApplet.applet.config;
+    this.userApplet = await this.userAppletsService.update(
+      this.userApplet._id,
+      updateApplet as UserApplet,
+    );
+  }
 }
