64162d0179cd763decb72506acffd2efe838c3ac
Yizhen Zhao <yizhen@uber.com>
2017-09-28 00:51:40 -0700

    Fix not redirect in production.

--- src/app/_interceptors/auth.interceptor.ts
+++ src/app/_interceptors/auth.interceptor.ts
@@ -2,6 +2,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/of';
 
+import * as _               from 'underscore';
 import { Observable }       from 'rxjs/Observable';
 import { Injectable }       from '@angular/core';
 import {
@@ -13,7 +14,7 @@ import {
   HttpResponse,
 }                           from '@angular/common/http';
 import { Router }           from '@angular/router';
-import { MatSnackBar }       from '@angular/material';
+import { MatSnackBar }      from '@angular/material';
 
 import { UserStateService } from '../_services';
 
@@ -33,7 +34,14 @@ export class AuthInterceptor implements HttpInterceptor {
       withCredentials: true,
     })).catch((err: any) => {
       if (err instanceof HttpErrorResponse) {
-        if (err.status === 401 && err.error.message === 'require login') {
+        let error: any;
+        if (_.isString(err.error)) {
+          error = JSON.parse(err.error);
+        } else {
+          error = err.error;
+        }
+
+        if (err.status === 401 && error.message === 'require login') {
           this.userState.remove();
           this.router.navigate(['/login']);
           return Observable.of(null);
